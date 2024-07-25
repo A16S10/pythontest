acc50aa0affdcfa899d6e8aff54007c12b38214b
Arshad Shaikh <arshad.shaikh@calsoftinc.com>
2024-07-25 16:11:24 +0530

    Initial commit

---
+++ list.py
@@ -0,0 +1,9 @@
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+
+print(thislist)
+
+print("Print length of the list:",len(thislist))
+
+print("print type of the list:",type(thislist))
+
+print(thislist[1:4])
